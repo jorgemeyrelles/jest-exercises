b1410781ce366f8e9e18bef0b1cb51471a959ca1
jorgemeyrelles <jotaengpuc@gmail.com>
2022-08-17 08:35:47 -0300

    feature: first commit

---
+++ test/gilded_rose.test.js
@@ -0,0 +1,85 @@
+const {Shop, Item} = require("../src/gilded_rose");
+
+describe("Gilded Rose", function() {
+  it("should foo", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("foo");
+  });
+  
+  it("SellIn decreasing one", function() {
+    const gildedRose = new Shop([new Item("foo", 1, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(0);
+  });
+
+  it("Quality decreasing one", function() {
+    const gildedRose = new Shop([new Item("foo", 1, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+  it("Quality can not be lower than zero", function() {
+    const gildedRose = new Shop([new Item("foo", 1, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+  it("Quality do decrease one if sellIn is on the time", function() {
+    const gildedRose = new Shop([new Item("foo", 1, 5)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(4);
+  });
+
+  it("Quality do decrease twice as days passed way over 2 expirate day", function() {
+    const gildedRose = new Shop([new Item("foo", -1, 5)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(3);
+  });
+
+  it("The difference of quality changing sellIn date", function() {
+    const gildedRoseOnTime = new Shop([new Item("foo", 1, 5)]);
+    const itemsOnTime = gildedRoseOnTime.updateQuality();
+    const gildedRoseOverTime = new Shop([new Item("foo", -1, 5)]);
+    const itemsOverTime = gildedRoseOverTime.updateQuality();
+    expect(itemsOnTime[0].quality - itemsOverTime[0].quality).toBe(1);
+  });
+
+  it("Quality can not be negative", function() {
+    const gildedRose = new Shop([new Item("foo", -1, 5)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(3);
+  });
+
+  it("Quality of aged Brie increase one more as the days pass way", function() {
+    const gildedRoseOneDay = new Shop([new Item("Aged Brie", 1, 1)]);
+    const itemsOneDay = gildedRoseOneDay.updateQuality();
+    const gildedRoseFourDays = new Shop([new Item("Aged Brie", 4, 1)]);
+    const itemsFourDays = gildedRoseFourDays.updateQuality();
+    expect(itemsOneDay[0].quality).toBe(2);
+    expect(itemsFourDays[0].quality).toBe(2);
+  });
+
+  it("Quality of any product can not be more than 50", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 70)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBeLessThan(50);
+  });
+
+  it("Quality of Sulfuras do not change for any sellIn value", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 5, 7)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(7);
+  });
+
+  it("Quality of Backstage passes increase as the days pass way", function() {
+    const backstage = "Backstage passes to a TAFKAL80ETC concert";
+    const gildedRoseTenDay = new Shop([new Item(backstage, 10, 1)]);
+    const itemsTenDay = gildedRoseTenDay.updateQuality();
+    const gildedRoseFiveDays = new Shop([new Item(backstage, 5, 1)]);
+    const itemsFiveDays = gildedRoseFiveDays.updateQuality();
+    expect(itemsTenDay[0].quality).toBe(3);
+    expect(itemsFiveDays[0].quality).toBe(4);
+  });
+
+});
