69bd0b0db43556ca617c48db9f19315ae6c4b422
jorgemeyrelles <jotaengpuc@gmail.com>
2022-09-14 10:11:28 -0300

    fix: added arr to get answer

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -6,28 +6,33 @@ class Shop {
   }
 
   updateQuality() {
+    const arrResponse = new Array();
     for (let item of this.items) {
       const { name, sellIn, quality } = item;
       switch (item.name) {
         case 'Aged Brie':
           const agedBrie = new AgedBrie(name, sellIn, quality);
           item = agedBrie.update();
-          return [item];
+          arrResponse.push(item);
+          break;
         case 'Backstage passes to a TAFKAL80ETC concert':
           const backstage = new Backstage(name, sellIn, quality);  
           item = backstage.update();
-          return [item];
+          arrResponse.push(item);
+          break;
         case 'Sulfuras, Hand of Ragnaros':
           const sulfuras = new Sulfuras(name, sellIn, quality);
           item = sulfuras.update();
-          return [item];
+          arrResponse.push(item);
+          break;
         default:
           const def = new Default(name, sellIn, quality);
           item = def.update();
-          return [item];
+          arrResponse.push(item);
+          break;
       }
     }
-    return this.items;
+    return arrResponse;
   }
 }
 
