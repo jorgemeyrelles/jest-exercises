aac2afde8e2170a62541b5c5f2cb4630dfe4b2a9
Mateus Resende <mateus.resende.ext@sweetgreen.com>
2022-09-20 20:21:04 -0300

    feat: create item map

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,5 +1,11 @@
 const { AgedBrie, Backstage, Sulfuras, Default } = require('./classes');
 
+const ITEM_MAP = {
+  'Aged Brie': AgedBrie,
+  'Backstage passes to a TAFKAL80ETC concert': Backstage,
+  'Sulfuras, Hand of Ragnaros': Sulfuras
+};
+
 class Shop {
   constructor(items=[]){
     this.items = items;
@@ -8,20 +14,13 @@ class Shop {
   updateQuality() {
     return this.items.map((item) => {
       const { name, sellIn, quality } = item;
-      switch (item.name) {
-        case 'Aged Brie':
-          const agedBrie = new AgedBrie(name, sellIn, quality);
-          return agedBrie.update();
-        case 'Backstage passes to a TAFKAL80ETC concert':
-          const backstage = new Backstage(name, sellIn, quality);  
-          return backstage.update();
-        case 'Sulfuras, Hand of Ragnaros':
-          const sulfuras = new Sulfuras(name, sellIn, quality);
-          return sulfuras.update();
-        default:
-          const def = new Default(name, sellIn, quality);
-          return def.update();
+      if (ITEM_MAP.hasOwnProperty(name)) {
+        const specialItem = new ITEM_MAP[name](name, sellIn, quality);
+        return specialItem.update();
       }
+
+      const def = new Default(name, sellIn, quality);
+      return def.update();
     });
   }
 }
