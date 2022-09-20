5c1dfa1efef859aa087e4ff69f00ee80d17f0ee6
jorgemeyrelles <jotaengpuc@gmail.com>
2022-09-20 20:15:29 -0300

    fix: using map

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -6,33 +6,23 @@ class Shop {
   }
 
   updateQuality() {
-    const arrResponse = new Array();
-    for (let item of this.items) {
+    return this.items.map((item) => {
       const { name, sellIn, quality } = item;
       switch (item.name) {
         case 'Aged Brie':
           const agedBrie = new AgedBrie(name, sellIn, quality);
-          item = agedBrie.update();
-          arrResponse.push(item);
-          break;
+          return agedBrie.update();
         case 'Backstage passes to a TAFKAL80ETC concert':
           const backstage = new Backstage(name, sellIn, quality);  
-          item = backstage.update();
-          arrResponse.push(item);
-          break;
+          return backstage.update();
         case 'Sulfuras, Hand of Ragnaros':
           const sulfuras = new Sulfuras(name, sellIn, quality);
-          item = sulfuras.update();
-          arrResponse.push(item);
-          break;
+          return sulfuras.update();
         default:
           const def = new Default(name, sellIn, quality);
-          item = def.update();
-          arrResponse.push(item);
-          break;
+          return def.update();
       }
-    }
-    return arrResponse;
+    });
   }
 }
 
